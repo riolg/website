593760710cd816cdba80bcce2f08e2113dd3496e
riolg <rio.lee.garcia@gmail.com>
2026-07-11 16:52:28 -0400

    fixed square

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const canvas = document.getElementById('gameCanvas');
-const ctx = canvas.getContext('2d');
+window.onload = function() {
+    const canvas = document.getElementById('gameCanvas');
+    const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = '#3498db';
+    const shapeSize  = 150;       //Width and height of the square
+    const shapeColor = '#3498db';
 
-ctx.fillRect(100, 100, 200, 200);
+    //keep it centered
+    const startX = (canvas.width - shapeSize) / 2;
+    const startY = (canvas.height - shapeSize) / 2;
+
+    ctx.fillStyle = shapeColor;
+    ctx.fillRect(startX, startY, shapeSize, shapeSize);
+};
