fbf43b781ff8ca2309621caa22d5a4a805a0423a
riolg <rio.lee.garcia@gmail.com>
2026-07-11 16:56:10 -0400

    fixed script.js

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ window.onload = function() {
     const canvas = document.getElementById('gameCanvas');
     const ctx = canvas.getContext('2d');
 
-    const shapeSize  = 150;       //Width and height of the square
-    const shapeColor = '#3498db';
 
-    //keep it centered
+    const shapeSize   = 150;       // Size of square
+    const shapeColor  = '#3498db'; // Color of square
+    const framePadding = 40;       // Extra space around it
+    // ==========================================
+
+    canvas.width = shapeSize + (framePadding * 2);
+    canvas.height = shapeSize + (framePadding * 2);
+
     const startX = (canvas.width - shapeSize) / 2;
     const startY = (canvas.height - shapeSize) / 2;
 
